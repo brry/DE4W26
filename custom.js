ee937b3e13056b33c15937e1632f1c0285f673cd
brry <berryboessenkool@hotmail.com>
2026-04-10 17:27:54 +0200

    link to overview added

--- custom.js
+++ custom.js
@@ -3,4 +3,10 @@ window.addEventListener("load", function () {
   img.src = "externalfig/HPI_logo.png";
   img.className = "hpi-logo";
   document.body.appendChild(img);
+
+  var link = document.createElement("a");
+  link.href = "index.html";
+  link.textContent = "← DE4W tutorial overview";
+  link.className = "course-link";
+  document.body.appendChild(link);
 });
